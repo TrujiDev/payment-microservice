021beef5d12236922f9f24d19ec1f11c8628aba9
TrujiDev <eetgrisales@gmail.com>
2024-05-22 15:18:01 -0500

    Added validation to new environment variables.

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,12 +4,18 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   STRIPE_SECRET_KEY: string;
+  ENDPOINTSECRET: string;
+  SUCCESS_URL: string;
+  CANCEL_URL: string;
 }
 
 const envSchema = joi
   .object({
     PORT: joi.number().required(),
     STRIPE_SECRET_KEY: joi.string().required(),
+    ENDPOINTSECRET: joi.string().required(),
+    SUCCESS_URL: joi.string().required(),
+    CANCEL_URL: joi.string().required(),
   })
   .unknown(true);
 
@@ -24,4 +30,7 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   stripeSecretKey: envVars.STRIPE_SECRET_KEY,
+  endpointSecret: envVars.ENDPOINTSECRET,
+  successUrl: envVars.SUCCESS_URL,
+  cancelUrl: envVars.CANCEL_URL,
 };
