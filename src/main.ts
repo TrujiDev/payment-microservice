c302e3051a55886eb1d55c509534ea3a86f1cce3
TrujiDev <eetgrisales@gmail.com>
2024-05-22 15:23:17 -0500

    Add rawBody option to NestFactory and configure global ValidationPipe.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('PaymentMicroservice');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(envs.port);
 
   logger.log(`Payment Microservice Running on port ${envs.port}`);
